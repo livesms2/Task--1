76025140cba078afb9712143a517e9eddb09af18
shamim <live_sms2@yahoo.com>
2023-10-21 04:22:17 +0500

    https://github.com/livesms2/task-1.git

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+// 1. Write a function that displays current date & time in your browser
+
+/*var rightNow = new Date();
+console.log(rightNow)*/
+
+// 2. Write a function that takes first & last name and then it greets the user using his full name.
+
+/*var name1 = "Muhammad";
+var name2 = "Shamim";
+var result = "Muhammad + Shamim";
+
+console.log(name1+name2);*/
+
+// 3. Write a function that adds two numbers (input by user) and returns the sum of two numbers
+
+/*function additionFunction(a, b) { 
+    return a + b; 
+} 
+  
+let num1 = 55; 
+let num2 = 100; 
+let sum = additionFunction(num1, num2); 
+console.log("Sum of given numbers is :", sum);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
